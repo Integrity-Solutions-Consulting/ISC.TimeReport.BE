5538fa714268a8ad75c7e31f4f2ed2d332450a90
jvivas-rock <jvivas.trecool@gmail.com>
2025-06-19 15:33:42 -0500

    Tests Skeletons

---
+++ isc.time.report.be/isc.time.report.be.tests/Controllers/v1/Persons/PersonControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using isc.time.report.be.api.Controllers.v1.Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isc.time.report.be.api.Controllers.v1.Persons.Tests
+{
+    public class PersonControllerTests
+    {
+        [Fact()]
+        public void PersonControllerTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetAllTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CreatePersonTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void UpdateLeaderTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
