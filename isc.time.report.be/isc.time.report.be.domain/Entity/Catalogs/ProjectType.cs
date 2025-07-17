517a4835738a3c44a17dc810de0b9a412cd04a3c
DI3GOD2507 <diego.cede.19@hotmail.com>
2025-07-17 12:36:08 -0500

    Arreglo de Project y agragado nuevas tabla catalogo projectType

---
+++ isc.time.report.be/isc.time.report.be.domain/Entity/Catalogs/ProjectType.cs
@@ -0,0 +1,15 @@
+﻿using isc.time.report.be.domain.Entity.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isc.time.report.be.domain.Entity.Catalogs
+{
+    public class ProjectType : BaseEntity
+    {
+        public string TypeName { get; set; } = null!;
+        public string? SubType { get; set; }
+    }
+}
