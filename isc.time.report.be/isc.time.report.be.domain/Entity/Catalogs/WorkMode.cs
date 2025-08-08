77cd88b8cd1f373e45f1bc028908ecd3c3c91734
JostinBasurtoM <jositn.basurto@integritysolutions.com.ec>
2025-08-08 16:11:32 -0500

    cambios a las entidades null

--- isc.time.report.be/isc.time.report.be.domain/Entity/Catalogs/WorkMode.cs
+++ isc.time.report.be/isc.time.report.be.domain/Entity/Catalogs/WorkMode.cs
@@ -10,6 +10,6 @@ namespace isc.time.report.be.domain.Entity.Catalogs
     public class WorkMode : BaseEntity
     {
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; } = null;
     }
 }
