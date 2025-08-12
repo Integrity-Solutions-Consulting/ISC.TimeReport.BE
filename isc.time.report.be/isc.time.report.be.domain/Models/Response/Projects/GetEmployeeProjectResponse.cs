0d334ef12f22d3a5aecb9fd754ef5d8b177eee38
JostinBasurtoM <jositn.basurto@integritysolutions.com.ec>
2025-08-12 08:57:03 -0500

    hotfix, de empeladoProyectoDto

--- isc.time.report.be/isc.time.report.be.domain/Models/Response/Projects/GetEmployeeProjectResponse.cs
+++ isc.time.report.be/isc.time.report.be.domain/Models/Response/Projects/GetEmployeeProjectResponse.cs
@@ -14,8 +14,8 @@ namespace isc.time.report.be.domain.Models.Response.Projects
         public int? EmployeeID { get; set; }
         public int? SupplierID { get; set; }
         public string AssignedRole { get; set; }
-        public decimal CostPerHour { get; set; }
-        public decimal AllocatedHours { get; set; }
+        public decimal? CostPerHour { get; set; }
+        public decimal? AllocatedHours { get; set; }
         public int ProjectID { get; set; }
         public bool Status { get; set; }
     }
