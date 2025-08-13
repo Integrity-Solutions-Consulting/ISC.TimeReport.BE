4e9d3fe8d782abcae224ca0a9fef692b9267f854
JostinBasurtoM <jositn.basurto@integritysolutions.com.ec>
2025-08-13 16:34:42 -0500

    hotfix y validaciones

--- isc.time.report.be/isc.time.report.be.application/Services/Holidays/HolidayServices.cs
+++ isc.time.report.be/isc.time.report.be.application/Services/Holidays/HolidayServices.cs
@@ -4,6 +4,7 @@ using isc.time.report.be.application.Interfaces.Repository.Holidays;
 using isc.time.report.be.application.Interfaces.Repository.PermissionTypes;
 using isc.time.report.be.application.Interfaces.Service.Holidays;
 using isc.time.report.be.domain.Entity.Holidays;
+using isc.time.report.be.domain.Exceptions;
 using isc.time.report.be.domain.Models.Request.Holidays;
 using isc.time.report.be.domain.Models.Response.Catalogs;
 using isc.time.report.be.domain.Models.Response.Holidays;
@@ -32,7 +33,7 @@ namespace isc.time.report.be.application.Services.Holidays
         public async Task<GetHolidayByIdResponse>GetHolidayByIdAsync(int id)
         {
             if (id <= 0)
-                throw new ArgumentException("El ID debe ser mayor a 0");
+                throw new ClientFaultException("El ID debe ser mayor a 0");
             var entity = await holidayRepository.GetHolidayByIdAsync(id);
             if (entity == null) throw new Exception("El feriado no existe");
             var holiday = _mapper.Map<GetHolidayByIdResponse>(entity);
