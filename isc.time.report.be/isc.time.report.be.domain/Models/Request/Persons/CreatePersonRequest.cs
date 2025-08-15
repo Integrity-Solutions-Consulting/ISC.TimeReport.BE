4e84f7204978184ec19d7bc162dc2945e5b742aa
Integrity Solutions <135142661+ISC-Fabrica@users.noreply.github.com>
2025-08-15 00:14:18 -0500

    Update CreatePersonRequest.cs

--- isc.time.report.be/isc.time.report.be.domain/Models/Request/Persons/CreatePersonRequest.cs
+++ isc.time.report.be/isc.time.report.be.domain/Models/Request/Persons/CreatePersonRequest.cs
@@ -11,13 +11,14 @@ namespace isc.time.report.be.domain.Models.Request.Persons
         public int? GenderID { get; set; }
         public int? NationalityId { get; set; }
         public int? IdentificationTypeId { get; set; }
-        public string IdentificationNumber { get; set; }
-        public string PersonType { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateOnly? BirthDate { get; set; }
+        public string? IdentificationNumber { get; set; } // <-- nullable
+        public string? PersonType { get; set; }            // <-- nullable
+        public string? FirstName { get; set; }             // <-- nullable
+        public string? LastName { get; set; }              // <-- nullable
+        public DateOnly? BirthDate { get; set; }           // <-- nullable
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
+
     }
 }
