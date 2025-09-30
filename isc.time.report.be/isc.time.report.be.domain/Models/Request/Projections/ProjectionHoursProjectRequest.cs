ab4a37a3c1dad62361231f12b1f2b03a3d0dc74f
JostinBasurtoM <jositn.basurto@integritysolutions.com.ec>
2025-09-30 14:42:29 -0500

    request de projection

---
+++ isc.time.report.be/isc.time.report.be.domain/Models/Request/Projections/ProjectionHoursProjectRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isc.time.report.be.domain.Models.Request.Projections
+{
+    public class ProjectionHoursProjectRequest
+    {
+    public int ProjectionHoursProjectId { get; set; }
+    public int ResourceTypeId { get; set; }
+    public string ResourceTypeName { get; set;}
+    public string resource_name { get; set; }
+    public decimal hourly_cost { get; set; }
+    public int resource_quantity { get; set; }
+    public string time_distribution { get; set; }
+    public decimal total_time { get; set; }
+    public decimal resource_cost { get; set; }
+    public decimal participation_percentage { get; set; }
+    public bool period_type { get; set; }
+    public int period_quantity { get; set; }
+    public int ProjectID { get; set; }
+    }
+}
