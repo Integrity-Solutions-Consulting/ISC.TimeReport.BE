9528d0c9ad2ea7b362deb052f6da530f85a0e8b3
Javier Avilés <javier.avils@integritysolutions.com.ec>
2025-12-08 14:27:21 -0500

    servicio de los sp de reporte

---
+++ isc.time.report.be/isc.time.report.be.application/Interfaces/Service/Reportes/IReporteService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static isc.time.report.be.domain.Models.Response.Reporte.ReporteRespose;
+
+namespace isc.time.report.be.application.Interfaces.Service.Reportes
+{
+    public interface IReporteService
+    {
+        Task<List<ReporteRecursosPorProyectoDto>> ObtenerReporteProyectoAsync();
+        Task<List<CantidadRecursoHoraClienteDto>> ObtenerReporteClienteAsync();
+
+    }
+}
---
+++ isc.time.report.be/isc.time.report.be.application/Services/Reportes/ReporteService.cs
@@ -0,0 +1,35 @@
+﻿using isc.time.report.be.application.Interfaces.Repository.Reportes;
+using isc.time.report.be.application.Interfaces.Service.Reportes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static isc.time.report.be.domain.Models.Response.Reporte.ReporteRespose;
+
+namespace isc.time.report.be.application.Services.Reportes
+{
+    public class ReporteService : IReporteService
+    {
+        private readonly IReportesRepository _repo;
+
+        public ReporteService(IReportesRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<List<ReporteRecursosPorProyectoDto>> ObtenerReporteProyectoAsync()
+        {
+            var data = await _repo.ObtenerReporteRecursosAsync();
+            return data;
+
+        }
+
+        public async Task<List<CantidadRecursoHoraClienteDto>> ObtenerReporteClienteAsync()
+        {
+            var data = await _repo.ObtenerCantidadRecursosYHorasPorClienteAsync();
+            return data;
+        }
+
+    }
+}
