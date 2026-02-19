cdea14c093768fc67731a0cf62b52c35297b5c84
Leonardo21 <leoalava2016@gmail.com>
2026-02-19 09:27:45 -0500

    Implementación de encriptación aletoria

--- isc.time.report.be/isc.time.report.be.domain/Models/Request/EncryptedRequest/EncryptedRequest.cs
+++ isc.time.report.be/isc.time.report.be.domain/Models/Request/EncryptedRequest/EncryptedRequest.cs
@@ -9,5 +9,6 @@ namespace isc.time.report.be.domain.Models.Request.EncryptedRequest
     public class EncryptedRequest
     {
         public string Data { get; set; } = string.Empty;
+        public string Iv { get; set; } = "";
     }
 }
